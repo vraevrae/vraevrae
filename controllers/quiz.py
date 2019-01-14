f298d5e3b7a504ef969c689df2f00f1a72dc6e72
Zaturrby <rjkorteschiel@gmail.com>
2019-01-14 12:49:53 +0100

    model

---
+++ controllers/quiz.py
@@ -0,0 +1,33 @@
+class Quiz ():
+    self.id = highestId + 1
+    self.quizvragen = [QuizQuestion(question) for question in range(10)]
+    self.users = [User(1), User(2)]
+    isStarted = False
+    activeQuestion = "34856389465"
+
+    def nextQuestion()
+
+    def finished()
+
+
+class QuizQuestion():
+    tempQuestion = API.createQuestion()
+
+    self.id = random()
+    self.question = tempQuestion.question
+    self.answers = [Answers() for awnser in tempQuestion.answers]
+    self.timer = 10
+
+    self.quiz = "843756387465"
+
+
+class User():
+    self.id = random()
+    self.name = form.name
+    self.score = 1
+    isOwner = False
+
+    def updateScore():
+
+
+nieuwQuiz = Quiz()
