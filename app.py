3816faf246cdb0cab804d71ef898e326c74c8db2
Zaturrby <rjkorteschiel@gmail.com>
2019-01-14 12:18:52 +0100

    some styles for form

--- app.py
+++ app.py
@@ -6,12 +6,12 @@ app = Flask(__name__)
 
 # ensure responses aren't cached
 if app.config["DEBUG"]:
-	@app.after_request
-	def after_request(response):
-		response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-		response.headers["Expires"] = 0
-		response.headers["Pragma"] = "no-cache"
-		return response
+    @app.after_request
+    def after_request(response):
+        response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+        response.headers["Expires"] = 0
+        response.headers["Pragma"] = "no-cache"
+        return response
 
 # configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_FILE_DIR"] = mkdtemp()
@@ -22,8 +22,9 @@ Session(app)
 
 @app.route('/', methods=["GET", "POST"])
 def index():
-	if request.method == "GET":
-		return render_template("index.html")
-	else:
-		# TODO
-		return render_template("index.html")
+    if request.method == "GET":
+        return render_template("index.html")
+
+    else:
+        # TODO
+        return render_template("index.html")
