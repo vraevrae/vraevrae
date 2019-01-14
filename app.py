d461af29039ec0badc8fd33f0dafede6cb4eb6ba
Yunus Demir <contact.github@yunusdemir.nl>
2019-01-14 11:56:51 +0100

    Updated code for index

--- app.py
+++ app.py
@@ -1,6 +1,29 @@
-from flask import Flask
+from flask import Flask, flash, redirect, render_template, request, session, url_for
+from flask_session import Session
+from tempfile import mkdtemp
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-        return "Hello, world!"
+# ensure responses aren't cached
+if app.config["DEBUG"]:
+	@app.after_request
+	def after_request(response):
+		response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+		response.headers["Expires"] = 0
+		response.headers["Pragma"] = "no-cache"
+		return response
+
+# configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+
+@app.route('/', methods=["GET", "POST"])
+def index():
+	if request.method == "GET":
+		return render_template("index.html")
+	else:
+		# TODO
+		return render_template("index.html")
