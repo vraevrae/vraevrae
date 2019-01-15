ef0aac392e450a4553dfd5c259f4b4deb4d4f9f0
Racheldehaan <rachel.dehaan12@gmail.com>
2019-01-15 14:26:54 +0100

    timer
    elke seconden nieuw teken, elke 10 sec wordt de pagina herladen en alle tekens verwijderd

--- Javascript/timer.js
+++ Javascript/timer.js
@@ -1,8 +1,14 @@
 var time = document.getElementById("time");
 
-function showPage(){
-    window.location.href = 'test2.html';
+function plusOne()
+{
+    time.appendChild(document.createTextNode('I'));
 }
 
+function showPage(){
+    window.location.href = 'test.html';
+}
+
+window.setInterval(plusOne, 1000);
 setTimeout(showPage, 10000);
 
