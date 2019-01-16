dc26e2bc60c4e1e1728d2b8e68815f4fc81719e1
Yunus Demir <contact.github@yunusdemir.nl>
2019-01-16 15:14:26 +0100

    Updated datasource

--- models/datasource.py
+++ models/datasource.py
@@ -23,12 +23,16 @@ class Datasource:
             self.source = opentdb.OpenTDB()
 
         # save questions to cache_data
-        self.cache_data = self.get_all_questions()
+        self.update_cache_data()
 
     @staticmethod
     def get_datasources() -> list:
         """Return all possible datasources (as noted in config)"""
         return config.POSSIBLE_DATASOURCES
+
+    def update_cache_data(self) -> None:
+        """Save questions to cache_data"""
+        self.cache_data = self.get_all_questions()
 
     def get_all_questions(self) -> list:
         """Function that returns all questions (formatted)"""
@@ -37,5 +41,9 @@ class Datasource:
     def get_question(self, current_question=current_question) -> dict:
         """Function that returns a question (that should not have been send in the current session)"""
         current_question = current_question % len(self.cache_data)
+        self.current_question += 1
+
+        if len(self.cache_data) - 5 == self.current_question:
+            self.update_cache_data()
 
         return self.cache_data[current_question]
