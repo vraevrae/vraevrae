8655b5fd0ba8a83cb5464f6468eb6fc4bce6c06d
DeniztheMenace <46484275+DeniztheMenace@users.noreply.github.com>
2019-01-16 16:39:13 +0100

    app.py
    app.py

--- app.py
+++ app.py
@@ -19,18 +19,19 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+
 @app.route('/', methods=["GET", "POST"])
 def index():
     return render_template("index.html")
 
 @app.route('/lobby', methods=["GET", "POST"])
 def lobby():
-    return render_template("lobby.html")
+    return render_template("lobby.html", users=[{"name": "Username 1", "score": 6590}, {"text": "Username 1", "score": 6590}, {"text": "Username 1", "score": 6590}, {"text": "Username 1", "score": 6590}])
 
 @app.route('/quiz', methods=["GET", "POST"])
 def quiz():
-    return render_template("quiz.html")
+    return render_template("quiz.html", question={"text": "What is the biggest planet?"}, answers=[{"text": "sun"}, {"text": "jupiter"}])
 
 @app.route('/scoreboard', methods=["GET", "POST"])
 def scoreboard():
-    return render_template("scoreboard.html", users=[])
+    return render_template("scoreboard.html", users=[{"name": "Username 1", "score": 6590}, {"text": "Username 1", "score": 6590}, {"text": "Username 1", "score": 6590}, {"text": "Username 1", "score": 6590}])
