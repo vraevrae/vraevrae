dd0ca873fa01bf506a0898d5aa7c8587a0fe238d
Yunus Demir <contact.github@yunusdemir.nl>
2019-01-17 11:51:32 +0100

    Question works now with the Datasource class

---
+++ models/test_datasource.py
@@ -0,0 +1,11 @@
+from models.app import App
+from models.datasource import Datasource
+
+
+def test_create_question_from_source():
+    """questions can be added form a source"""
+    app = App()
+    quiz_id = app.store.create_quiz(Datasource)
+    question_id = app.store.create_question_from_source(quiz_id)
+    question = app.store.questions[question_id]
+    assert question
