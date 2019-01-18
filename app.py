e7b8b430880b0ece5001cd926d05c5b14ff259f7
Yunus Demir <contact.github@yunusdemir.nl>
2019-01-18 12:35:01 +0100

    fixing app.py and lobby.html

--- app.py
+++ app.py
@@ -3,8 +3,11 @@ from tempfile import mkdtemp
 from flask import Flask, render_template, request, session, redirect, url_for
 from flask_session import Session
 
-import config
-from models import app as quizapp
+from models.datasource import Datasource
+from models.store import Store
+from models.view import View
+
+store = Store()
 
 app = Flask(__name__)
 
@@ -24,49 +27,104 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 
+def get_view(self, user_id):
+    user = self.store.get_user_by_id(user_id)
+    quiz = self.store.get_quiz_by_id(user.quiz)
+
+    if not quiz.is_started:
+        return View("lobby", {"users": self.store.get_users_by_id(quiz.users)})
+
+    if quiz.is_started and not quiz.is_finished:
+        question_id = quiz.get_current_question_id()
+        question = self.store.get_question_by_id(question_id)
+        answers = self.store.get_answers_by_id(question.answers)
+        return View("question", {"question": question, "answers": answers})
+
+    if quiz.is_finished:
+        return View("scoreboard", {"users": self.store.get_users_by_id(quiz.users)})
+
+
+def start_quiz(user_id):
+    user = store.get_user_by_id(user_id)
+
+    if user.is_owner:
+        return store.get_quiz_by_id(user.quiz).start()
+
+
+def answer_question(user_id, answer_id):
+    answer = store.get_answer_by_id(answer_id)
+    user = store.get_user_by_id(user_id)
+
+    if answer.is_correct:
+        question = store.get_question_by_id(answer.question_id)
+        user.score += question.score
+        return True
+
+    return False
+
+
 @app.route('/', methods=["GET", "POST"])
 def index():
     if request.method == "GET":
         return render_template("index.html")
 
     elif request.method == "POST":
+        username = request.form["username"]
+
         if request.form["newgame"]:
-            if request.form["username"] == "":
+            if username == "":
                 return render_template("index.html", error="Username should not be empty!")
 
-            session["user_id"] = quizapp.App.new_quiz(request.form["username"], config.DEFAULT_DATASOURCE)
+            quiz_id = store.create_quiz(Datasource)
+            for _ in range(10):
+                store.create_question_from_source(quiz_id)
+
+            user_id = store.create_user(quiz_id=quiz_id, name=username, is_owner=True)
+
+            session["user_id"] = user_id
 
             return redirect(url_for("game"))
+
         elif request.form["joingame"]:
-            if request.form["username"] == "":
+            gamecode = request.form["gamecode"]
+
+            if username == "":
                 return render_template("index.html", error="Username should not be empty!")
-            elif request.form["gamecode"] == "":
+            elif gamecode == "":
                 return render_template("index.html", error="Game code should not be empty!")
 
-            session["user_id"] = quizapp.App.join_quiz(request.form["username"], request.form["gamecode"])
+            quiz = store.get_quiz_by_code(gamecode)
+            user_id = store.create_user(quiz_id=quiz.quiz_id, name=username, is_owner=False)
+
+            session["user_id"] = user_id
+
+            return redirect(url_for("game"))
 
 
 @app.route('/game', methods=["GET", "POST"])
 def game():
-    return render_template("quiz.html")
+    if request.method == "GET":
+        user = store.get_user_by_id(session["user_id"])
+        quiz = store.get_quiz_by_id(user.quiz)
 
+        if not quiz.is_started:
+            return render_template("lobby.html", users=store.get_users_by_id(quiz.users), owner=user.is_owner)
 
-@app.route('/lobby', methods=["GET", "POST"])
-def lobby():
-    owner=True
-    return render_template("lobby.html",
-                           users=[{"name": "Username 1", "score": 6590}, {"text": "Username 1", "score": 6590},
-                                  {"text": "Username 1", "score": 6590}, {"text": "Username 1", "score": 6590}], owner=owner)
+        if quiz.is_started and not quiz.is_finished:
+            question_id = quiz.get_current_question_id()
+            question = store.get_question_by_id(question_id)
+            answers = store.get_answers_by_id(question.answers)
+            return render_template("quiz.html", question=question, answers=answers)
 
+        if quiz.is_finished:
+            return render_template("scoreboard.html", users=store.get_users_by_id(quiz.users))
 
-@app.route('/quiz', methods=["GET", "POST"])
-def quiz():
-    return render_template("quiz.html", question={"text": "What is the biggest planet?"},
-                           answers=[{"text": "sun"}, {"text": "jupiter"}])
+    elif request.method == "POST":
+        print("POST")
+        action = request.form["action"]
+        user = store.get_user_by_id(session["user_id"])
 
+        if action == "start" and user.is_owner:
+            store.get_quiz_by_id(user.quiz).start()
 
-@app.route('/scoreboard', methods=["GET", "POST"])
-def scoreboard():
-    return render_template("scoreboard.html",
-                           users=[{"name": "Username 1", "score": 6590}, {"text": "Username 1", "score": 6590},
-                                  {"text": "Username 1", "score": 6590}, {"text": "Username 1", "score": 6590}])
+            return redirect(url_for("game"))
