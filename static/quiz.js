6785bc4d5241366d1e42d5856319e172613bdded
Racheldehaan <rachel.dehaan12@gmail.com>
2019-01-22 16:21:30 +0100

    quiz en scoreboard
    layout scoreboard en refreshen quiz deel1

--- static/quiz.js
+++ static/quiz.js
@@ -1,11 +1,18 @@
 $(function() {
     let current_progress = 100;
+    
     let interval = setInterval(function () {
         current_progress -= 10;
         $("#quiz-progress")
         .css("width", `${current_progress}%`)
         .attr("aria-valuenow", current_progress);
-        if (current_progress === 0)
+
+        if (current_progress === 0){
             clearInterval(interval);
+            window.location.href = window.location.pathname + window.location.search + window.location.hash;
+        }
     }, 1000);
   });
+
+
+
