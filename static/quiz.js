cd3744d5d56ced1223ecbfcdcf4283eb0d52c899
Zaturrby <rjkorteschiel@gmail.com>
2019-01-23 15:16:50 +0100

    fix ajax call buttons fix #21

--- static/quiz.js
+++ static/quiz.js
@@ -1,34 +1,35 @@
 $(document).ready(function() {
-    let current_progress = 100;
-    
-    let interval = setInterval(function () {
-        current_progress -= 10;
-        $("#quiz-progress")
-        .css("width", `${current_progress}%`)
-        .attr("aria-valuenow", current_progress);
+  let current_progress = 100
 
-        if (current_progress === 0){
-            /*location.reload();*/
-            clearInterval(interval);
-        }
-    }, 1000);
+  let interval = setInterval(function() {
+    current_progress -= 10
+    $('#quiz-progress')
+      .css('width', `${current_progress}%`)
+      .attr('aria-valuenow', current_progress)
 
-    $("#.answerbtn").click(function(e) {
-        console.log("hallo");
-        var url = "/game"; // the script where you handle the form input.
-        console.log($("#update").serialize());
-        // $.ajax({
-        //     type: "POST",
-        //     url: url,
-        //     data: {'data':$("#update").serialize()}, // serializes the form's elements.
-        //     success: function()
-        //     {
-        //         document.getElementById("update").innerHTML = "HALLO"
-        //         var elem = document.getElementById('button');
-        //         elem.parentNode.removeChild(elem);
-        //     }
-        //     });
+    if (current_progress === 0) {
+      /*location.reload();*/
+      clearInterval(interval)
+    }
+  }, 1000)
 
-        return false; // avoid to execute the actual submit of the form.
-    });
-});
+  $('.answerbtn').click(function(e) {
+    console.log('answerbtn run')
+
+    data = {
+      action: 'answer',
+      answer_id: e.currentTarget.value
+    }
+
+    $.ajax({
+      type: 'POST',
+      url: '/game',
+      data: data, // serializes the form's elements.
+      success: function() {
+        document.getElementById('update').innerHTML = 'HALLO'
+      }
+    })
+
+    return false // avoid to execute the actual submit of the form.
+  })
+})
