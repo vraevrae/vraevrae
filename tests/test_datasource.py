a570ecfbc42ef0b6b605abb8d2a8e4486cbf10c9
Zaturrby <rjkorteschiel@gmail.com>
2019-01-24 13:40:52 +0100

    fix last test file

--- tests/test_datasource.py
+++ tests/test_datasource.py
@@ -1,11 +1,11 @@
-# TODO FIX FILE, ALL COMMENTED BECAUSE MODELS/APP IS DELETED
-#
-# from models.datasource import Datasource
-# from models.store import Store
-#
-# def test_create_question_from_real_source():
-#     """questions can be added form a source"""
-#     quiz_id = Store.create_quiz(Datasource)
-#     question_id = Store.create_question_from_source(quiz_id)
-#     question = Store.questions[question_id]
-#     assert question
+from models.datasource import Datasource
+from models.store import Store
+
+
+def test_create_question_from_real_source():
+    """questions can be added form a source"""
+    store = Store()
+    quiz_id = store.create_quiz(Datasource)
+    question_id = store.create_question_from_source(quiz_id)
+    question = store.questions[question_id]
+    assert question
