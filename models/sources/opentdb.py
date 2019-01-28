066888930cde5d11a07da1d6ca6ab954e19e8281
Yunus Demir <contact.github@yunusdemir.nl>
2019-01-28 11:05:11 +0100

    Completed difficulty

--- models/sources/opentdb.py
+++ models/sources/opentdb.py
@@ -21,15 +21,16 @@ class OpenTDB:
         # try to get a correct request from Open Trivia DB
         try:
 
-            if difficulty != None:
+            if difficulty is not None:
                 # do request to Open Trivia DB API and format to JSON
-                r = requests.get("https://opentdb.com/api.php?amount=" + str(amount_of_questions) + "&difficulty=" + str(difficulty) +
-                                "&type=multiple")
-                json = r.json()
+                r = requests.get("https://opentdb.com/api.php?amount=" + str(
+                    amount_of_questions) + "&difficulty=" + str(difficulty) +
+                                 "&type=multiple")
             else:
                 r = requests.get("https://opentdb.com/api.php?amount=" + str(amount_of_questions) +
-                                "&type=multiple")
-                json = r.json()
+                                 "&type=multiple")
+
+            json = r.json()
 
             # check if request was correct
             if json["response_code"] == 0:
