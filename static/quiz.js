94ff88ef01fc96f8aaad4ad9b45e508c7d8b59b7
Yunus Demir <contact.github@yunusdemir.nl>
2019-01-28 11:14:03 +0100

    cleaned quiz.js

--- static/quiz.js
+++ static/quiz.js
@@ -1,36 +1,36 @@
 $(document).ready(function() {
-  let current_progress = 100
+    let current_progress = 100;
 
   let interval = setInterval(function() {
-    current_progress -= 10
+      current_progress -= 10;
     $('#quiz-progress')
       .css('width', `${current_progress}%`)
-      .attr('aria-valuenow', current_progress)
+        .attr('aria-valuenow', current_progress);
 
     if (current_progress === 0) {
       location.reload();
       clearInterval(interval)
     }
-  }, 1000)
+  }, 1000);
 
   $('.answerbtn').click(function(e) {
 
     data = {
       action: 'answer',
       answer_id: e.currentTarget.value
-    }
+    };
 
     $.ajax({
       type: 'POST',
       url: '/game',
       data: data, // serializes the form's elements.
       success: function() {
-        var d1 = document.getElementById('update');
+          let d1 = document.getElementById('update');
         d1.insertAdjacentHTML('afterend', '<div id="updated" class="center"><h3 class="prj-name">Wait for the next question...</h3></div>');
         d1.parentNode.removeChild(d1);
       }
-    })
+    });
 
     return false // avoid to execute the actual submit of the form.
   })
-})
+});
