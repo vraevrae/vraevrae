7268fec263145a77b5c4d0a7319796be04b14620
Zaturrby <rjkorteschiel@gmail.com>
2019-01-29 14:49:33 +0100

    comment

--- app.py
+++ app.py
@@ -150,9 +150,10 @@ def game():
     if request.method == "GET":
         # retrieve the question and answers
         answers = store.get_answers_by_id(question.answers)
-        number = quiz.current_question + 1
+        # get current question (indexed to 1 instead of 0)
+        readable_current_question = quiz.current_question + 1
 
-        return render_template("quiz.html", question=question, answers=answers, number=number)
+        return render_template("quiz.html", question=question, answers=answers, number=readable_current_question)
 
     elif request.method == "POST":
         answer_id = request.form["answer_id"]
