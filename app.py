8ed3308f41803ea22c18c2f19057d94cf081f250
Zaturrby <rjkorteschiel@gmail.com>
2019-01-30 21:03:38 +0100

    working and refactored quiz

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ from helpers.helpers import user_required, game_mode_required
 from models.sources.opentdb import OpenTDB
 from models.store import store
 from models.useranswer import UserAnswer
+import json
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "extemelysecretvraevraesocketkey"
@@ -301,7 +302,18 @@ def get_current_question(data):
 
     print({"question": question.text, "answers": answers})
 
+<<<<<<< HEAD
     emit("current_question", {"question": question, "answers": answers}, room=data["quiz_id"])
+=======
+    quiz_object = {
+        "start_time": quiz.start_time.isoformat(),
+        "max_questions": quiz.max_questions,
+        "max_time_in_seconds": quiz.max_time_in_seconds
+    }
+
+    emit("current_question", {"question": vars(
+        question), "answers": answers, "quiz": quiz_object}, room=data["quiz_id"])
+>>>>>>> working and refactored quiz
 
 
 @socketio.on('send_answer')
@@ -322,13 +334,15 @@ def set_answer(data):
 
         # get the users answers for this question (user is still scoped to quiz, so user ==
         # quiz)
-        user_answers = store.get_user_answers_by_user_and_question_id(user_id, answer.question_id)
+        user_answers = store.get_user_answers_by_user_and_question_id(
+            user_id, answer.question_id)
 
         # if correct and no previous answer found and the question is still active
         if answer.is_correct and len(user_answers) < 1 and answer.question_id == question_id:
             print("ANSWER IS CORRECT")
             # create a new answer
-            new_user_answer = UserAnswer(answer.question_id, answer_id, user_id)
+            new_user_answer = UserAnswer(
+                answer.question_id, answer_id, user_id)
 
             # store new answer and increment the store
             store.set_user_answer(new_user_answer)
@@ -338,7 +352,8 @@ def set_answer(data):
         print("QID -> ", quiz_id)
         print("RID ->", request.sid)
 
-        emit("received_answer", {"success": True, "user_id": user_id}, room=quiz_id)
+        emit("received_answer", {"success": True,
+                                 "user_id": user_id}, room=quiz_id)
 
     else:
         pass
--- static/quiz.js
+++ static/quiz.js
@@ -1,4 +1,5 @@
-var vue_question_app = new Vue({
+// vue setup
+let vue_question_app = new Vue({
   el: '#vue_question',
   data: {
     question: {},
@@ -7,97 +8,39 @@ var vue_question_app = new Vue({
   }
 })
 
-// create socket connection with server
-let socket, user_id, quiz_id, progress_timer
-
-// create interval, so the progressbar can be updated every second
-socket = io.connect('http://' + document.domain + ':' + location.port)
-
-// get data from the html file
-quiz_id = get_data('quiz_id')
-user_id = get_data('user_id')
+// socket setup
+let socket = io.connect('http://' + document.domain + ':' + location.port)
+let { quiz_id, user_id } = document.querySelector('#data').dataset
 
 // if socket connected succesfully
 socket.on('connect', function() {
-  console.log('Socket connected')
-  console.log('quiz_id ->', quiz_id)
-
-  // join game with current quiz_id
-  socket.emit('join_game', { quiz_id: quiz_id, user_id: user_id }) // COMMENTED BECAUSE LOBBY
-  // ALREADY ADDS
-  // TO QUIZ
-  get_current_question(quiz_id, user_id)
+  console.log('[SOCKET_IO]: Socket connected')
+  socket.emit('join_game', { quiz_id, user_id })
+  get_current_question()
 })
 
-// if socket receives a message from the server
-socket.on('message', function(message) {
-  console.log('SOCKET MESSAGE: ', message)
-})
+// function to get current question from server
+function get_current_question() {
+  console.log('[SOCKET_IO]: GET NEW QUESTION')
+  socket.emit('get_current_question', { quiz_id, user_id })
+}
 
 // if socket receives new question from the server
 socket.on('current_question', function(data) {
+  console.log('[SOCKET_IO]: CURRENT QUESTION: ', data)
   vue_question_app.answers = data.answers
   vue_question_app.question = data.question
-  console.log('CURRENT QUESTION: ', data)
 })
-
-// if server received answer
-socket.on('received_answer', function(data) {
-  vue_question_app.question = 'Waiting for next question'
-  vue_question_app.answers = []
-  console.log('RECEIVED ANSWER SUCCESS', data)
-})
-
-// function to replace document.getElementById() (so code is better readable)
-function get_el(id) {
-  return document.getElementById(id)
-}
-
-// function to replace document.getElementById().dataset (so code is better readable)
-function get_data(name) {
-  return get_el('data').dataset[name]
-}
-
-// function to update timer every second
-function update_timer() {
-  console.log('update_timer')
-  // get current progress
-  let current_progress = get_el('progress_bar').value
-
-  // update current_progress
-  current_progress -= 10
-
-  // set new progress
-  get_el('progress_bar').value = current_progress
-
-  if (current_progress === 0) {
-    console.log('PROGRESS === 0, NEW QUESTION')
-    get_current_question()
-  }
-
-  return true
-}
-
-// function to get current question from server
-function get_current_question(
-  quiz_id = get_data('quiz_id'),
-  user_id = get_data('user_id')
-) {
-  console.log('GET NEW QUESTION (quiz_id, user_id)', quiz_id, user_id)
-  socket.emit('get_current_question', { quiz_id: quiz_id, user_id: user_id })
-}
 
 // function to send answer to the server
 function send_answer(answer_id) {
-  let user_id = get_data('user_id')
-  let quiz_id = get_data('quiz_id')
-
-  console.log('SEND ANSWER', answer_id)
-
-  // if the button is clicked, send answer via socket to the server
-  socket.emit('send_answer', {
-    user_id: user_id,
-    answer_id: answer_id,
-    quiz_id: quiz_id
-  })
+  console.log('[SOCKET_IO]: SEND ANSWER', answer_id)
+  socket.emit('send_answer', { user_id, answer_id, quiz_id })
 }
+
+// if server received answer
+socket.on('received_answer', function(data) {
+  console.log('[SOCKET_IO]: RECEIVED ANSWER SUCCESS', data)
+  vue_question_app.question.text = 'Waiting for next question'
+  vue_question_app.answers = []
+})
