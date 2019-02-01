4c9d7644a2e88b62788f3f67a55a8045390c99fe
Yunus Demir <contact.github@yunusdemir.nl>
2019-02-01 13:19:52 +0100

    updated app.py

--- app.py
+++ app.py
@@ -340,4 +340,4 @@ def set_answer(data):
                 question = store.get_question_by_id(answer.question_id)
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port="5001")
+    socketio.run(app, host='0.0.0.0', port=5001)
